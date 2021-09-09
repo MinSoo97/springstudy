30662438422fff0a8cc67c356bb7a578962d7052
MinSoo97 <kms33233@gmail.com>
2021-09-09 12:28:21 +0900

    회원 서비스 연습

--- src/main/java/com/example/test/service/MemberService.java
+++ src/main/java/com/example/test/service/MemberService.java
@@ -3,15 +3,19 @@ package com.example.test.service;
 import com.example.test.domain.Member;
 import com.example.test.repository.MemberRepository;
 import com.example.test.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     // private final MemberRepository memberRepository = new MemoryMemberRepository(); //이렇게 하면 test랑 본 코드랑 다른 memorymemberrepositoy를 사용한다.
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
